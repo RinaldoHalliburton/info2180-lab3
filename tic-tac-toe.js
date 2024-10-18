7a38fccba44f394056312c549a77d09b2c758a5e
Rinaldo <rinaldohalliburton7917@gmail.com>
2024-10-18 17:22:16 -0500

    Change the style when you move your mouse

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -15,6 +15,12 @@ function loadSquares() //function to load squares in board
     square.addEventListener('click', function() {
          handleSquareClick(square);
         });
+    square.addEventListener('mouseover',function(){
+        square.classList.add('hover');
+    })
+    square.addEventListener('mouseout',function(){
+        square.classList.remove('hover');
+    })
     });
 }
 
